5521b768678cea933b295a075b02c14f09b81498
Eren Çolak <93825099+erenxcolakx@users.noreply.github.com>
2024-02-02 21:25:07 +0300

    js files added

---
+++ public/script.js
@@ -0,0 +1,89 @@
+// ------------------------- User Time Update -------------------------//
+function updateUserTime() {
+    // Get the user's local time
+    const userLocalTime = new Date();
+
+    // Format the time
+    const hours = userLocalTime.getHours();
+    const minutes = userLocalTime.getMinutes();
+
+    const formattedTime = `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+
+    // Display the time on the page
+    $("#time").text(formattedTime);
+}
+
+// Call the function initially
+updateUserTime();
+
+// Update the user's time every second (1000 milliseconds)
+setInterval(updateUserTime, 5000);
+
+// ------------------------ City Search and Find -------------------------//
+
+async function fetchAndDisplaySuggestions() {
+const inputBox = $("#citySearch");
+const userInput = inputBox.val().trim();
+
+if (userInput.length === 0) {
+  $("#exampleCities").html('');
+  return;
+}
+
+try {
+  const response = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${userInput}&count=50&language=en&format=json`);
+  const suggestions = response.data.results;
+
+  // Clear previous suggestions
+  $("#exampleCities").html('');
+
+  // Create and append individual suggestion elements
+  suggestions.forEach(result => {
+    const cityName = result.name;
+    const country = result.admin1 == null ? "" : result.admin1;
+    const id = result.id;
+    const latitude = result.latitude;
+    const longitude = result.longitude;
+
+    const suggestionElement = $(`<li></li>`)
+      .attr({
+        "id": id,
+        "latitude": latitude,
+        "longitude": longitude,
+      })
+      .text(`${cityName}, ${country}`);
+
+    $("#exampleCities").append(suggestionElement);
+  });
+
+  // Receive updated list of suggestions
+
+  // Add click event listener to each suggestion
+  $("#exampleCities li").on('click', async function () {
+    const selectedItem = $(this).text();
+    const latitude = $(this).attr("latitude");
+    const longitude = $(this).attr("longitude");
+    const city = selectedItem.split(',')[0].trim();
+    $("#exampleCities").html(''); // Clean suggestions list
+    inputBox.val("");
+    // Assuming latitude, longitude, and city are variables containing your data
+    const queryString = `?latitude=${latitude}&longitude=${longitude}&cityName=${city}`;
+    // Redirect to /weather with query parameters
+    window.location.href = '/weather' + queryString;
+  });
+
+} catch (error) {
+  console.error(error);
+  $("#exampleCities").html('<li style="color: red;"> Could not find the city. Please try again.</li>');
+}
+}
+
+$("#citySearch").on('input', fetchAndDisplaySuggestions);
+
+const citySuggestions = $("#exampleCities li"); // unorderedList items
+
+const API_URL = "https://api.open-meteo.com/v1/forecast?";
+const current =["temperature_2m","apparent_temperature","rain,snowfall","weather_code","wind_speed_10m","relative_humidity_2m"];
+const daily =["weather_code","temperature_2m_max","temperature_2m_min,sunshine_duration"];
+const timezone = "auto";
+const forecast_days = 1;
