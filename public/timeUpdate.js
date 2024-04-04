bd4b8d45682abb18c5c1e0f027362af78664c463
Eren Çolak <93825099+erenxcolakx@users.noreply.github.com>
2024-04-04 16:16:32 +0300

    timeUpdate fixed

--- public/timeUpdate.js
+++ public/timeUpdate.js
@@ -8,15 +8,26 @@ function updateTimeString() {
 
   // Get hour and minute values
   let hour = dateObject.getHours();
-  let minute = (dateObject.getMinutes() + 1) % 60;
-  if (minute === 0) {
-    hour++;
+  let minute = (dateObject.getMinutes()) % 60;
+
+  // Check if it's past midnight (00:00)
+  if (hour === 23 && minute === 59) {
+    // If it's 23:59, set hour to 0 and minute to 0 (00:00)
+    hour = 0;
+    minute = 0;
+  } else {
+    // Otherwise, increment the minute
+    if (minute === 59) {
+      hour = (hour + 1) % 24;
+      minute = 0;
+    } else {
+      minute++;
+    }
   }
+
   // Update the time value in the HTML content
   $("#time").html(`${hour < 10 ? '0' : ''}${hour}:${minute < 10 ? '0' : ''}${minute}`);
-  console.log(hour, minute);
 }
 
 // Update the time value by calling the updateTimeString function every second
 setInterval(updateTimeString, 60000);
-
