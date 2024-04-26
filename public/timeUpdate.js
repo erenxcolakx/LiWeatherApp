ccb650419b51469961bd91f80b69449270293eb9
Eren Çolak <93825099+erenxcolakx@users.noreply.github.com>
2024-04-27 01:01:51 +0300

    Update timeUpdate.js

--- public/timeUpdate.js
+++ public/timeUpdate.js
@@ -1,32 +1,23 @@
 function updateTimeString() {
   // Get the current time string from the HTML content
-  const timeString = $("#time").text();
+  let timeString = $("#time").text();
 
-  // Convert the time string in 'HH:mm' format to 'YYYY-MM-DDTHH:mm' format
-  const dateTimeString = `1970-01-01T${timeString}`;
-  const dateObject = new Date(dateTimeString);
+  const [hours, minutes] = timeString.split(':').map(Number);
 
-  // Get hour and minute values
-  let hour = dateObject.getHours();
-  let minute = (dateObject.getMinutes()) % 60;
+  // Create currentDate object
+  const currentDate = new Date();
 
-  // Check if it's past midnight (00:00)
-  if (hour === 23 && minute === 59) {
-    // If it's 23:59, set hour to 0 and minute to 0 (00:00)
-    hour = 0;
-    minute = 0;
-  } else {
-    // Otherwise, increment the minute
-    if (minute === 59) {
-      hour = (hour + 1) % 24;
-      minute = 0;
-    } else {
-      minute++;
-    }
-  }
+  currentDate.setHours(hours);
+  currentDate.setMinutes(minutes);
+
+  // Increment time by one minute
+  currentDate.setTime(currentDate.getTime() + 60000);
+
+  let hour = currentDate.getHours();
+  let minute = currentDate.getMinutes();
 
   // Update the time value in the HTML content
-  $("#time").html(`${hour < 10 ? '0' : ''}${hour}:${minute < 10 ? '0' : ''}${minute}`);
+  $("#time").html(`${hour}:${minute < 10 ? '0' : ''}${minute}`);
 }
 
 // Update the time value by calling the updateTimeString function every second
