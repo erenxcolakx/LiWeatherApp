9e5ac79b6b30724e886573bc6997bd0c6390096d
Eren Çolak <93825099+erenxcolakx@users.noreply.github.com>
2024-08-28 01:52:50 +0300

    vercel address update

--- client/src/services/weatherApi.js
+++ client/src/services/weatherApi.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+const API_URL = process.env.NODE_ENV === 'production'
+  ? 'https://li-weather-app.vercel.app'
+  : 'http://localhost:5000';
+
 const getWeatherData = async (latitude, longitude) => {
     try {
-      const response = await axios.get('/api/weather', {
+      const response = await axios.get(`${API_URL}/api/weather`, {
         params: {
           latitude,
           longitude,
